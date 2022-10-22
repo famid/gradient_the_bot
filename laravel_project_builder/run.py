9f82371f193f250e4330347880ea397549bef668
famid <ahsanulhoquefamid@gmail.com>
2022-10-22 20:40:58 +0600

    Modified | added two function for database credentials

--- laravel_project_builder/run.py
+++ laravel_project_builder/run.py
@@ -1,7 +1,24 @@
 from laravel_project_builder.project_builder import ProjectBuilder
+import os
+
+database_credentials = {
+    'DB_DATABASE': '',
+    'DB_USERNAME': '',
+    'DB_PASSWORD': ''
+}
+
+
+def set_database_credential() -> None:
+    for credential in database_credentials:
+        os.environ[credential] = input(f"{credential}: ")
 
 
 def build_laravel_project(project_name: str):
+    try:
+        set_database_credential()
+    except Exception as err:
+        return dict(success=False, message=str(err))
+
     laravel_app = ProjectBuilder(project_name)
 
     laravel_app_build_response = laravel_app.build_project()
@@ -9,4 +26,3 @@ def build_laravel_project(project_name: str):
         return laravel_app_build_response
 
     return laravel_app_build_response
-    pass
